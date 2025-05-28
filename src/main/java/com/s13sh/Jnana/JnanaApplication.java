7ff701154e2b821fb8122b33d01b5c65ab1d1295
sanat-buyya <sanatbuyya@gmail.com>
2025-05-28 16:59:40 +0530

    Project Setup

---
+++ src/main/java/com/s13sh/Jnana/JnanaApplication.java
@@ -0,0 +1,13 @@
+package com.s13sh.Jnana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JnanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s13sh/Jnana/JnanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s13sh.Jnana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
